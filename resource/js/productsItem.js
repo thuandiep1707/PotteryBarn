97948e5d505e7e6d7cf371a69fb7c47683e49c84
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-01-22 04:11:41 +0700

    continue

--- resource/js/productsItem.js
+++ resource/js/productsItem.js
@@ -1,10 +1,27 @@
-        fetch('../resource/js/item.json')
-            .then(res => res.json())
-            .then(data =>{
-                const item = data;
-                for (var num = 0; item[num] != undefined; num++ ){
-                    let img = item[num].img;
-                    let name = item[num].name;
-                    document.getElementById("item").innerHTML += '<div class="item" Onclick ="callListItem('+num+')"><img src="../resource/image/product/'+img+'" alt="'+name+'"><p>'+name+'</p></div>'
-                }
-            })
+fetch("../resource/js/itemGroup.json")
+  .then((res) => res.json())
+  .then((data) => {
+    let item = data;
+    for (var num = 0; item[num] != undefined; num++) {
+      let img = item[num].img;
+      let name = item[num].name;
+      document.getElementById("item").innerHTML +=
+        '<div class="item" Onclick ="callListItem(\''+ name +'\')"><img src="../resource/image/product/'+ img +'" alt="'+ name +'"><p>'+ name +"</p></div>";
+    }
+  });
+ function callListItem(name){
+    document.getElementById('link').innerHTML += ' / '+ name;
+    document.getElementById('item').innerHTML = '';
+    console.log(name);
+    fetch("../resource/js/"+name+".json")
+        .then(res => res.json())
+        .then((data)=>{
+            let item = data;
+            for (var num = 0; data[num] != undefined; num++){
+                let img = data[num].img;
+                let name = data[num].name;
+                document.getElementById('item').innerHTML += 
+                '<div class="item" Onclick ="callItem(\''+ name +'\')"><img src="../resource/image/product/glazed/'+ img +'" alt="'+ name +'"><p>'+ name +"</p></div>";
+            }
+        })
+ }

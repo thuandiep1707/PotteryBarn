7bdc9502062d3ebf8df8cbd4e3e7b805493e36ff
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-01-22 04:40:46 +0700

    continue

--- resource/js/productsItem.js
+++ resource/js/productsItem.js
@@ -1,27 +1,54 @@
-fetch("../resource/js/itemGroup.json")
-  .then((res) => res.json())
-  .then((data) => {
-    let item = data;
-    for (var num = 0; item[num] != undefined; num++) {
-      let img = item[num].img;
-      let name = item[num].name;
-      document.getElementById("item").innerHTML +=
-        '<div class="item" Onclick ="callListItem(\''+ name +'\')"><img src="../resource/image/product/'+ img +'" alt="'+ name +'"><p>'+ name +"</p></div>";
-    }
-  });
- function callListItem(name){
-    document.getElementById('link').innerHTML += ' / '+ name;
-    document.getElementById('item').innerHTML = '';
-    console.log(name);
-    fetch("../resource/js/"+name+".json")
-        .then(res => res.json())
-        .then((data)=>{
-            let item = data;
-            for (var num = 0; data[num] != undefined; num++){
-                let img = data[num].img;
-                let name = data[num].name;
-                document.getElementById('item').innerHTML += 
-                '<div class="item" Onclick ="callItem(\''+ name +'\')"><img src="../resource/image/product/glazed/'+ img +'" alt="'+ name +'"><p>'+ name +"</p></div>";
-            }
-        })
- }
+function callback() {
+  document.getElementById("item").innerHTML = "";
+  fetch("../resource/js/itemGroup.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let item = data;
+      for (var num = 0; item[num] != undefined; num++) {
+        let img = item[num].img;
+        let name = item[num].name;
+        document.getElementById("item").innerHTML +=
+          '<div class="item" Onclick ="callListItem(\'' +
+          name +
+          '\')"><img src="../resource/image/product/' +
+          img +
+          '" alt="' +
+          name +
+          '"><p>' +
+          name +
+          "</p></div>";
+        document.getElementById("nav").innerHTML +=
+          "<li Onclick=\"callListItem('" + name + "')\" >" + name + "</li>";
+      }
+    });
+}
+
+callback();
+
+function callListItem(name) {
+  document.getElementById("link").innerHTML =
+    '<h2 onclick="callback()">PRODUCTS</h2>' + name;
+  document.getElementById("item").innerHTML = "";
+  fetch("../resource/js/" + name + ".json")
+    .then((res) => res.json())
+    .then((data) => {
+      let item = data;
+      for (var num = 0; data[num] != undefined; num++) {
+        let img = data[num].img;
+        let name = data[num].name;
+        document.getElementById("item").innerHTML +=
+          '<div class="item" Onclick ="callItem(' +
+          num +
+          ')"><img src="../resource/image/product/glazed/' +
+          img +
+          '" alt="' +
+          name +
+          '"><p>' +
+          name +
+          "</p></div>";
+      }
+    });
+}
+function callItem(name){
+
+}

8c3df7f33b1cbc62e1621d51f3d561c46574b919
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-01-22 21:03:59 +0700

    continue

--- resource/js/productsItem.js
+++ resource/js/productsItem.js
@@ -1,4 +1,6 @@
 function callback() {
+  document.getElementById("link").innerHTML =
+    '<h2 onclick="callback()">PRODUCTS</h2>'
   document.getElementById("item").innerHTML = "";
   fetch("../resource/js/itemGroup.json")
     .then((res) => res.json())
@@ -27,28 +29,34 @@ callback();
 
 function callListItem(name) {
   document.getElementById("link").innerHTML =
-    '<h2 onclick="callback()">PRODUCTS</h2>' + name;
+    '<h2 onclick="callback()">PRODUCTS</h2> <h4 onclick="callListItem(\''+name+'\')">'+ name+'</h4>';
   document.getElementById("item").innerHTML = "";
   fetch("../resource/js/" + name + ".json")
     .then((res) => res.json())
     .then((data) => {
-      let item = data;
       for (var num = 0; data[num] != undefined; num++) {
         let img = data[num].img;
-        let name = data[num].name;
+        let Nname = data[num].name;
         document.getElementById("item").innerHTML +=
           '<div class="item" Onclick ="callItem(' +
-          num +
+          num + ",'"+name+"'"+
           ')"><img src="../resource/image/product/glazed/' +
           img +
           '" alt="' +
-          name +
+          Nname +
           '"><p>' +
-          name +
+          Nname +
           "</p></div>";
       }
     });
 }
-function callItem(name){
-
+function callItem(num,name){
+  document.getElementById("item").innerHTML = "";
+  fetch("../resource/js/"+name+".json")
+  .then((res) => res.json())
+  .then((data) => {
+    let item =data;
+    alert("Thành công");
+    document.getElementById('item').innerHTML += "abcsaskdhkasjd";
+  });
 }
